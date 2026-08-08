5a4f177e93179932285fd7442d67c4382aafa319
Jhansi-1025 <jhansiguguloth25@gmail.com>
2026-08-08 14:25:14 +0530

    my commit

--- Factorial.java
+++ Factorial.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Factorial{
     // Recursive function to calculate factorial
@@ -11,15 +10,8 @@ public class Factorial{
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a non-negative integer: ");
-        int num = scanner.nextInt();
         
-        if (num < 0) {
-            System.out.println("Factorial is not defined for negative numbers.");
-        } else {
-            long result = calculate(num);
-            System.out.println("Factorial of " + num + " is: " + result);
-        }
-        scanner.close();
+            long result = calculate(5);
+            System.out.println("Factorial of 5" + " is: " + result);
     }
 }
