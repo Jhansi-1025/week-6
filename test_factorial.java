c177998da147ddbd3948ce6f28531effc7cc2a12
Jhansi-1025 <jhansiguguloth25@gmail.com>
2026-08-08 14:20:37 +0530

    my second commit

--- test_factorial.java
+++ test_factorial.java
@@ -1,7 +1,7 @@
 public class test_factorial{
     public static void main(String[] args){
         int n=5;
-        int result=Factorial.calculate(n);
+        long result=Factorial.calculate(n);
         System.out.println("result is"+result);
     }
 }
